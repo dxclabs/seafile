876d950ee57983cc836fd84ebc314bc15dc969dc
campbell <campbell@dxclabs.com>
2022-12-20 15:03:53 +0000

    disable log.conf in upstream.conf, remove unecessary config in entrypoint.sh

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -2,28 +2,13 @@
 
 set -e
 
-if [ "$1" = 'gunicorn-consul-template' ]; then
-    consul-template -template "config/config.py.tpl:app/config/config.py" -once
-    exec /bin/gunicorn "app:create_app()"
-fi
+COMMAND=${1}
+shift
 
-if [ "$1" = 'gunicorn' ]; then
-    exec /bin/gunicorn "app:create_app()" --workers 2 --chdir "/opt/app-root/" --bind :8000 --timeout 30
-fi
-
-if [ "$1" = 'runserver' ]; then
-    exec /usr/bin/flask run --host=0.0.0.0
-fi
-
-if [ "$1" = 'runserver-consul-template' ]; then
-    consul-template -template "config/config.py.tpl:app/config/config.py" -once
-    exec /usr/bin/flask run --host=0.0.0.0
-fi
-
-if [[ "$1" = 'seafile' ]]; then
+if [[ "$COMMAND" = 'seafile' ]]; then
     /opt/seafile/seafile-server-latest/seafile.sh start || true
     /opt/seafile/seafile-server-latest/seahub.sh start || true
     exec /usr/sbin/nginx -g 'daemon off;'
 fi
 
-exec "$@"
+exec "$COMMAND" "$@"
